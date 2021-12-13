7bc9774120c280521608123a896e759363bfe7eb
mcsoini <soini@posteo.de>
2021-12-13 22:24:30 +0100

    initial

---
+++ scheduler.py
@@ -0,0 +1,14 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+import datetime
+
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=5)
+# @sched.scheduled_job('interval', seconds=5)
+def timed_job():
+    with open("cron_test.dat", "a") as f:
+        f.writelines(["Test", str(datetime.datetime.now()), "\n"])
+
+sched.start()
+
