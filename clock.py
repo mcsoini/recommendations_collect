06c0628628459c3a6b00a8bb30d5b7c048aa0a9f
mcsoini <soini@posteo.de>
2021-12-14 22:45:58 +0100

    update test

--- clock.py
+++ clock.py
@@ -1,14 +1,13 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-import datetime
-
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=5)
-# @sched.scheduled_job('interval', seconds=5)
+@sched.scheduled_job('interval', seconds=3)
 def timed_job():
-    with open("cron_test.dat", "a") as f:
-        f.writelines(["Test", str(datetime.datetime.now()), "\n"])
+    print('This job is run every three minutes.')
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+def scheduled_job():
+    print('This job is run every weekday at 5pm.')
 
 sched.start()
-
