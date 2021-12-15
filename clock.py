f25e34c82bb1b5af00a25f85203323de2473a695
mcsoini <soini@posteo.de>
2021-12-15 14:19:42 +0100

    update

--- clock.py
+++ clock.py
@@ -4,10 +4,16 @@ import datetime
 
 sched = BlockingScheduler()
 
-# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=5)
-@sched.scheduled_job('interval', seconds=5)
-def timed_job():
-    print('This job is run regularly.')
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=15)
+#@sched.scheduled_job('interval', seconds=5)
+def schedule1_job():
+    print('This job is run at hour=15.')
+    with open("cron_test.dat", "a") as f:
+        f.writelines(["Test", str(datetime.datetime.now()), "\n"])
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=14, minute=30)
+def schedule2_job():
+    print('This job is at hour=14, minute=30.')
     with open("cron_test.dat", "a") as f:
         f.writelines(["Test", str(datetime.datetime.now()), "\n"])
 
